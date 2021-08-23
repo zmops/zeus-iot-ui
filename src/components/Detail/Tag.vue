99ce8901e22def2e94378c794a41f7ad95325f9b
xiaoyao <lee920817@hotmail.com>
2021-08-23 19:15:17 +0800

    优化设备详情-标签页面

---
+++ src/components/Detail/Tag.vue
@@ -0,0 +1,74 @@
+<!-- 标签组件 -->
+<template>
+  <div class="Tag">
+    <div v-for="(item,index) in list" :key="index" class="zeus-list-conten zeus-flex-between">
+      <div class="left zeus-flex-default">
+        <div class="zeus-list-item">
+          <el-input v-model="item.tag" size="mini">
+            <template slot="prepend">键</template>
+          </el-input>
+        </div>
+        <div class="zeus-list-item">
+          <el-input v-model="item.value" size="mini">
+            <template slot="prepend">值</template>
+          </el-input>
+        </div>
+      </div>
+      <i class="el-icon-delete zeus-icon" @click="del(index)" />
+    </div>
+    <el-button class="add-btn" plain icon="el-icon-plus" size="mini" @click="add">增加标签</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Tag',
+  props: {
+    tagList: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  data() {
+    return {
+      list: this.tagList
+    }
+  },
+  methods: {
+    del(i) {
+      this.list.splice(i, 1)
+      this.change()
+    },
+    add() {
+      for (const item of this.list) {
+        if (item.tag === '' || item.value === '') {
+          this.$message({
+            message: '请填写完整当前标签的键值',
+            type: 'warning'
+          })
+          return false
+        }
+      }
+      this.list.push({
+        tag: '',
+        value: ''
+      })
+      this.change()
+    },
+    change() {
+      this.$emit('change', this.list)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.Tag {
+  .add-btn{
+    width: 100%;
+    border-style: dashed;
+  }
+}
+</style>
--- src/views/deviceMgr/device/tag.vue
+++ src/views/deviceMgr/device/tag.vue
@@ -1,42 +1,52 @@
 <template>
-  <div>
-    <el-form ref="metadataForm" label-width="80px" label-position="top" class="dialog-form">
-      <el-form-item label="标签">
-        <div v-for="(item,index) in tagList" :key="'tag'+index" class="zeus-list-conten zeus-flex-between">
-          <div class="left zeus-flex-default">
-            <div class="zeus-list-item">
-              <el-input v-model="item.tag" size="mini">
-                <template slot="prepend">键</template>
-              </el-input>
-            </div>
-            <div class="zeus-list-item">
-              <el-input v-model="item.value" size="mini">
-                <template slot="prepend">值</template>
-              </el-input>
-            </div>
-          </div>
-          <i class="el-icon-delete zeus-icon" @click="delItem(index)" />
-        </div>
-        <el-button class="add-btn" plain icon="el-icon-plus" size="mini" @click="tagAdd">增加标签</el-button>
-        <div class="el-form-item-tips">
-          <i class="el-icon-info" />可在产品上增加自定义标签，以方便统计分析。
-        </div>
-      </el-form-item>
-    </el-form>
-    <div class="zeus-form-btns zeus-flex-center">
-      <el-button type="primary" size="mini" round @click="handleSubmit">确 定</el-button>
-      <el-button size="mini" round @click="handleCancle">取 消</el-button>
+  <div class="tag zeus-relative">
+    <div v-for="(item, index) in viewList" :key="index" class="tag-item zeus-inline-block">
+      <span class="tag-item-l zeus-inline-block">{{ item.tag }}</span>
+      <span class="tag-item-r zeus-inline-block">{{ item.value }}</span>
     </div>
+    <el-button type="text" icon="el-icon-edit" class="zeus-right zeus-absolute edit" @click="edit"></el-button>
+    <el-dialog
+      :visible.sync="dialogVisible"
+      :destroy-on-close="true"
+      :close-on-click-modal="false"
+      :close-on-press-escape="false"
+      :width="'700px'"
+      :show-close="false"
+    >
+      <div slot="title" class="dialog-title zeus-flex-between">
+        <div class="left">
+          <svg-icon icon-class="dialog_edit" />
+          编辑标签
+        </div>
+        <div class="right">
+          <svg-icon icon-class="dialog_close" class="closeicon" />
+          <svg-icon icon-class="dialog_onclose" class="closeicon" @click="dialogVisible = false" />
+        </div>
+      </div>
+      <div class="dialog-body">
+        <Tag :tag-list="tagList" @change="change" />
+      </div>
+      <el-footer class="dialog-footer-btn">
+        <el-button size="mini" round @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" size="mini" round @click="handleSubmit">确 定</el-button>
+      </el-footer>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 import { getDeviceTag, updateTag } from '@/api/deviceMgr'
+import Tag from '@/components/Detail/Tag'
 export default {
+  components: {
+    Tag
+  },
   data() {
     return {
       prodId: '',
-      tagList: []
+      tagList: [],
+      viewList: [],
+      dialogVisible: false
     }
   },
   async created() {
@@ -50,77 +60,72 @@ export default {
       getDeviceTag({ deviceId: this.prodId }).then(res => {
         if (res.code == 200) {
           this.tagList = res.data
+          this.viewList = JSON.parse(JSON.stringify(res.data))
         }
       })
     },
-    tagAdd() {
-      const obj = {
-        tag: '',
-        value: ''
-      }
-      if (this.tagList.length > 0) {
-        let rule = true
-        for (const item of this.tagList) {
-          if (item.tag === '') {
-            rule = false
-          }
-        }
-        if (rule) {
-          this.tagList.push(obj)
-        } else {
-          this.$message({
-            message: '请填写完整当前标签的键值',
-            type: 'warning'
-          })
-        }
-      } else {
-        this.tagList.push(obj)
-      }
+    change(list) {
+      this.tagList = list
     },
-    delItem(type, i) {
-      this.tagList.splice(i, 1)
-    },
-    async handleCancle() {
-      await this.getTagList()
+    edit() {
+      this.dialogVisible = true
+      this.tagList = JSON.parse(JSON.stringify(this.viewList))
     },
     handleSubmit() {
-      if (this.tagList.length == 1 && this.tagList[0].tag == '' && this.tagList[0].value == '') {
-        this.tagList = []
-      }
-      if (this.tagList.length > 0) {
-        let rule = true
-        for (const item of this.tagList) {
-          if (item.tag === '') {
-            rule = false
-          }
-        }
-        if (rule) {
-          this.tagUpdate()
-        } else {
+      for (const item of this.tagList) {
+        if (item.tag === '' || item.value === '') {
           this.$message({
             message: '请填写完整当前标签的键值',
             type: 'warning'
           })
+          return false
         }
-      } else {
-        this.tagUpdate()
       }
-    },
-    tagUpdate() {
-      const data = {
-        productId: this.prodId,
-        productTag: this.tagList
-      }
-      updateTag(data).then(res => {
+      updateTag({ productId: this.prodId, productTag: this.tagList }).then(res => {
         if (res.code == 200) {
           this.$message({
             message: '操作成功',
             type: 'success'
           })
           this.getTagList()
+          this.dialogVisible = false
         }
       })
     }
   }
 }
 </script>
+<style lang="scss" scoped>
+.tag {
+  width: 100%;
+  background-color: #fff;
+  padding: 17px 16px 4px 16px;
+
+  .tag-item{
+    font-size: 12px;
+    background-color: #E3E9EF;
+    border-radius: 2px;
+    margin-right: 40px;
+    margin-bottom: 12px;
+
+    .tag-item-l{
+      background-color: #3C435B;
+      color: #fff;
+      padding: 4px 8px;
+      margin-left: 2px;
+      border-radius: 2px;
+    }
+
+    .tag-item-r{
+      padding: 6px;
+      color: #242E42;
+    }
+  }
+
+  .edit{
+    right:18px ;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+}
+</style>
