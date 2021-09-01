57968994bc7b2d28cf430ed1b94f65bf11dd6c40
xiaoyao <lee920817@hotmail.com>
2021-09-01 17:25:25 +0800

    设备创建表单抽成组件

--- src/views/productMgr/product/device.vue
+++ src/views/productMgr/product/device.vue
@@ -16,79 +16,20 @@
       :close-on-press-escape="false"
       :width="'700px'"
       :show-close="false"
-      @close="close"
     >
       <div slot="title" class="dialog-title zeus-flex-between">
         <div class="left">
-          <svg-icon v-if="state === '创建'" icon-class="dialog_add" />
-          <svg-icon v-if="state === '编辑'" icon-class="dialog_edit" />
+          <svg-icon v-if="state === '创建'" icon-class="dialog_add"/>
+          <svg-icon v-if="state === '编辑'" icon-class="dialog_edit"/>
           {{ state }}设备
         </div>
         <div class="right">
-          <svg-icon icon-class="dialog_close" class="closeicon" />
-          <svg-icon icon-class="dialog_onclose" class="closeicon" @click="dialogVisible = false" />
+          <svg-icon icon-class="dialog_close" class="closeicon"/>
+          <svg-icon icon-class="dialog_onclose" class="closeicon" @click="dialogVisible = false"/>
         </div>
       </div>
       <div class="dialog-body">
-        <el-form ref="dialogForm" :rules="rules" :model="dialogForm" label-width="80px" label-position="top" class="dialog-form">
-          <el-form-item v-if="state === '编辑'" label="设备ID" prop="deviceId">
-            <el-input v-model="dialogForm.deviceId" size="mini" disabled/>
-          </el-form-item>
-          <el-form-item label="设备名称" prop="name">
-            <el-input v-model="dialogForm.name" size="mini"/>
-          </el-form-item>
-          <el-form-item label="产品" prop="productId">
-            <el-select v-model="dialogForm.productId" :disabled="'deviceId' in dialogForm" filterable placeholder="请选择产品" size="mini">
-              <el-option
-                v-for="item in productList"
-                :key="item.productId"
-                :label="item.name"
-                :value="item.productId"
-              />
-            </el-select>
-          </el-form-item>
-          <el-form-item label="设备组" prop="deviceGroupIds">
-            <el-select v-model="dialogForm.deviceGroupIds" multiple filterable placeholder="请选择设备组" size="mini">
-              <el-option
-                v-for="item in deviceGroup"
-                :key="item.deviceGroupId"
-                :label="item.name"
-                :value="item.deviceGroupId.toString()"
-              />
-            </el-select>
-          </el-form-item>
-          <el-form-item label="描述" prop="remark">
-            <el-input v-model="dialogForm.remark" type="textarea" rows="2" size="mini"/>
-          </el-form-item>
-          <el-form-item label="坐标" prop="position">
-            <el-input v-model="dialogForm.position" size="mini"/>
-          </el-form-item>
-          <el-form-item label="地址" prop="addr">
-            <el-input v-model="dialogForm.addr" size="mini"/>
-          </el-form-item>
-        </el-form>
-        <baidu-map
-          class="bm-view zeus-mt-20"
-          :zoom="15"
-          :center="center"
-          :ak="selfKey"
-          inertial-dragging
-          :scroll-wheel-zoom="true"
-          @ready="mapReady"
-          @click="selectPoint"
-        >
-          <bm-marker :position="point" :dragging="true" animation="BMAP_ANIMATION_BOUNCE"/>
-          <bm-geolocation
-            anchor="BMAP_ANCHOR_BOTTOM_RIGHT"
-            :show-address-bar="true"
-            :auto-location="true"/>
-          <bm-control class="map-input zeus-pl-10 zeus-pr-10 zeus-pt-10">
-            <bm-auto-complete v-model="keyword" :sug-style="{zIndex: 1}">
-              <el-input v-model="keyword" placeholder="请输入关键字" class="map-input"/>
-            </bm-auto-complete>
-          </bm-control>
-          <bm-local-search :keyword="keyword" :auto-viewport="true" :panel="false" @searchcomplete="search"/>
-        </baidu-map>
+        <deviceForm ref="deviceForm" v-model="dialogForm" :state="state" :product-list="productList" :device-group="deviceGroup"/>
       </div>
       <el-footer class="dialog-footer-btn">
         <el-button size="mini" round @click="dialogVisible = false">取 消</el-button>
@@ -102,8 +43,7 @@
 import BusinessTable from '@/components/Basics/BusinessTable'
 import SearchForm from '@/components/Basics/SearchForm'
 import Pagination from '@/components/Basics/Pagination'
-import BaiduMap from 'vue-baidu-map/components/map/Map.vue'
-import { BmGeolocation, BmLocalSearch, BmMarker, BmControl, BmAutoComplete } from 'vue-baidu-map'
+import deviceForm from '@/views/deviceMgr/device/deviceForm'
 import { createDevice, getDeviceByPage, getDeviceGrpList, updateDevice } from '@/api/deviceMgr'
 import { getDictListByCode } from '@/api/system'
 import { getProductList } from '@/api/porductMgr'
@@ -119,18 +59,7 @@ export default {
     BusinessTable,
     SearchForm,
     Pagination,
-    // 地图
-    BaiduMap,
-    // 手动定位控件
-    BmGeolocation,
-    // 检索控件
-    BmLocalSearch,
-    // marker控件
-    BmMarker,
-    // 自定义控件
-    BmControl,
-    // 自动填充
-    BmAutoComplete
+    deviceForm
   },
   data() {
     return {
@@ -344,69 +273,32 @@ export default {
       }
     },
     submit() {
-      this.$refs.dialogForm.validate(async(valid) => {
-        if (valid) {
-          if (this.dialogForm.deviceId) {
-            // updateDevice(this.dialogForm).then(async(res) => {
-            //   if (res.code == 200) {
-            //     this.$message({
-            //       message: '修改成功',
-            //       type: 'success'
-            //     })
-            //     this.dialogVisible = false
-            //     await this.getList()
-            //   }
-            // })
-          } else {
-            // createDevice(this.dialogForm).then(async(res) => {
-            //   if (res.code == 200) {
-            //     this.$message({
-            //       message: '添加成功',
-            //       type: 'success'
-            //     })
-            //     this.dialogVisible = false
-            //     await this.getList()
-            //   }
-            // })
-          }
+      if (this.$refs.deviceForm.validateForm()) {
+        if (this.dialogForm.deviceId) {
+          // updateDevice(this.dialogForm).then(async(res) => {
+          //   if (res.code == 200) {
+          //     this.$message({
+          //       message: '修改成功',
+          //       type: 'success'
+          //     })
+          //     this.dialogVisible = false
+          //     await this.getList()
+          //   }
+          // })
+        } else {
+          // createDevice(this.dialogForm).then(async(res) => {
+          //   if (res.code == 200) {
+          //     this.$message({
+          //       message: '添加成功',
+          //       type: 'success'
+          //     })
+          //     this.dialogVisible = false
+          //     await this.getList()
+          //   }
+          // })
         }
-      })
-    },
-    selectPoint({ type, target, point, pixel, overlay }) {
-      this.point = point
-      this.dialogForm.position = point.lng + ',' + point.lat
-    },
-    mapReady({ BMap, map }) {
-      const _this = this
-      if (_this.dialogForm.position) {
-        const arr = _this.dialogForm.position.split(',')
-        _this.center = {
-          lng: arr[0],
-          lat: arr[1]
-        }
-        _this.point = {
-          lng: arr[0],
-          lat: arr[1]
-        }
-      } else {
-        // 获取自动定位方法
-        const geolocation = new BMap.Geolocation()
-        // 获取自动定位获取的坐标信息
-        geolocation.getCurrentPosition(
-          function(r) {
-            _this.center = {
-              lng: r.point.lng,
-              lat: r.point.lat
-            }
-            _this.point = {
-              lng: r.point.lng,
-              lat: r.point.lat
-            }
-          },
-          { enableHighAccuracy: true }
-        )
       }
-    },
+    }
   }
 }
 </script>
