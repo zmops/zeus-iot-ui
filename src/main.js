a1254de7235a973b35ef86b05923dd327e583ba1
xiaoyao <lee920817@hotmail.com>
2021-09-24 15:29:51 +0800

    [fix]:Try to fix the problem that the page cannot be opened

--- src/main.js
+++ src/main.js
@@ -1,23 +1,18 @@
 import Vue from 'vue'
-
+import App from './App'
 import 'normalize.css/normalize.css' // A modern alternative to CSS resets
-
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import locale from 'element-ui/lib/locale/lang/en' // lang i18n
-
 import '@/styles/index.scss' // global css
 import '@/styles/common.scss'
 import '@/styles/element-variables.scss'
-import App from './App'
-import store from './store'
-import router from './router'
 import JsEncrypt from 'jsencrypt'
-
 import '@/icons' // icon
 import './utils/directives.js'
 import '@/permission' // permission control
-
+import router from './router'
+import store from './store'
 // set ElementUI lang to EN
 // Vue.use(ElementUI, { locale })
 // 如果想要中文版 element-ui，按如下方式声明
