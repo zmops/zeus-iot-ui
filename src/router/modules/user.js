87d97cd9bf92abdfc3c6b4a869dcf61343ded13b
xiaoyao <lee920817@hotmail.com>
2021-09-28 10:06:00 +0800

    [fix]:Modify menu

--- src/router/modules/user.js
+++ src/router/modules/user.js
@@ -1,20 +1,20 @@
 import Layout from '@/layout'
 /* 用户管理的路由 */
 const userRouter = {
-  path: '/system/userMgr',
+  path: '/userMgr',
   component: Layout,
   name: 'userMgr',
-  redirect: '/system/user',
+  redirect: '/userMgr/user',
   meta: {
     title: '用户管理',
-    icon: 'menu_system',
-    active_icon: 'menu_system_ac'
+    icon: 'menu_user',
+    active_icon: 'menu_user_ac'
   },
   children: [
     {
-      path: '/system/user',
+      path: '/userMgr/user',
       component: () => import('@/views/userManage/user'),
-      name: '/system/user',
+      name: '/userMgr/user',
       meta: {
         title: '用户',
         icon24: 'user24',
@@ -22,9 +22,9 @@ const userRouter = {
       }
     },
     {
-      path: '/system/userGroup',
+      path: '/userMgr/userGroup',
       component: () => import('@/views/userManage/userGroup'),
-      name: '/system/userGroup',
+      name: '/userMgr/userGroup',
       meta: {
         title: '用户组',
         icon24: 'userGroup24',
