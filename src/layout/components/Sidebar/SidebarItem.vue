c7d558b265c2dd2e5a17b64aefa3db875e0038e0
xiaoyao <lee920817@hotmail.com>
2021-09-28 15:39:17 +0800

    [fix]:Optimize menu permissions

--- src/layout/components/Sidebar/SidebarItem.vue
+++ src/layout/components/Sidebar/SidebarItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="!item.hidden">
+  <div v-if="!item.hidden && item.show">
     <template v-if="hasOneShowingChild(item.children,item) && (!onlyOneChild.children||onlyOneChild.noShowingChildren)&&!item.alwaysShow">
       <app-link v-if="onlyOneChild.meta" :to="resolvePath(onlyOneChild.path)">
         <el-menu-item :index="resolvePath(onlyOneChild.path)" :class="{'submenu-title-noDropdown':!isNest}">
